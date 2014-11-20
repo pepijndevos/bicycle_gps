6ba32a46a41914894100239fad7a39aaa90944ff
Pepijn de Vos <pepijndevos@gmail.com>
2014-11-20 19:10:40 +0100

    clean and improve main loop

--- bicycle_gps/bicycle_gps.ino
+++ bicycle_gps/bicycle_gps.ino
@@ -16,8 +16,7 @@
 #define TFT_DC 9
 #define TFT_CS 10
 #define SD_CS 4
-#define SPEEDOMETER 4 //int
-#define JOYSTICK 5 //int
+#define JOYSTICK 18
 #define JOYSTICK_X 14
 #define JOYSTICK_Y 15
 
@@ -30,14 +29,10 @@ Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 // Set up GPS
 Adafruit_GPS GPS(&Serial2);
 
-// keep track of speedometer
-volatile unsigned long last_cycle;
-volatile unsigned long cycle_time;
-
 // joystick center
 int x_centre, y_centre;
 double zoom = 10000;
-volatile bool streetnames = false;
+bool streetnames = false;
 
 // file system
 SdFat sd;
@@ -79,13 +74,8 @@ void setup()
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
   
-  // speedometer
-  attachInterrupt(SPEEDOMETER, speedometer, FALLING);
-  last_cycle = millis();
-  cycle_time = 1000;
-  
   // joystick
-  attachInterrupt(JOYSTICK, buttonpress, FALLING);
+  pinMode(JOYSTICK, INPUT);
   x_centre = analogRead(JOYSTICK_X);
   y_centre = analogRead(JOYSTICK_Y);
 
@@ -146,8 +136,12 @@ void loop()
   tft.println(" Pa");
   tft.print(temperature, 2);
   tft.println(" C");
-  tft.print(60000 / cycle_time);
-  tft.println(" RPM");
+  
+  tft.setCursor(250, 0);
+  tft.setTextSize(2);
+  tft.print(GPS.hour);
+  tft.print(":");
+  tft.print(GPS.minute);
 
   tft.setTextSize(3);
   tft.setCursor(100, 200);
@@ -155,15 +149,18 @@ void loop()
     tft.println(" Km/h");
 
   unsigned long time = millis() + 10000;
-  bool btn = streetnames;
-  while (millis() < time && btn == streetnames) {
+  bool streetnames_ = streetnames;
+  while (millis() < time && streetnames_ == streetnames) {
     int y = y_centre - analogRead(JOYSTICK_Y);
+    int btn = digitalRead(JOYSTICK);
     if (y > 20) {
       zoom *= 0.8;
       break;
     } else if (y < -20) {
       zoom *= 1.3;
       break;
+    } else if (btn == LOW) {
+      streetnames = !streetnames;
     }
   }
   /*tft.println("Joystick:");
@@ -187,17 +184,4 @@ double lon_scale(int32_t lat) {
 // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
 SIGNAL(TIMER0_COMPA_vect) {
   GPS.read();
-}
-
-void speedometer() {
-  unsigned long time = millis();
-  if (time - last_cycle > 100) {
-    cycle_time = time - last_cycle;
-    last_cycle = time;
-  }
-}
-
-void buttonpress() {
-  Serial.println("button");
-  streetnames = !streetnames;
 }
